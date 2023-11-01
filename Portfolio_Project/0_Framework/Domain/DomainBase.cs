9f1dc263c1503f2bfe6b33b3d77c05a258514ddd
Maral <mrlmosafercomputer@gmail.com>
2023-11-02 01:14:35 +0330

    Framework Domain Implemented

---
+++ Portfolio_Project/0_Framework/Domain/DomainBase.cs
@@ -0,0 +1,10 @@
+﻿namespace _0_Framework.Domain
+{
+    public class DomainBase
+    {
+        public long Id { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public DomainBase() => CreationDate = DateTime.Now;
+    }
+}
---
+++ Portfolio_Project/0_Framework/Domain/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace _0_Framework.Domain
+{
+    public interface IRepositoryBase<Model> where Model : class
+    {
+        Model GetBy(long id);
+        List<Model> GetList();
+        void CreateAndSave(Model entity);
+        void SaveChanges();
+        void CheckDataExists();
+        Expression<Func<Model, bool>> expression();
+    }
+}
