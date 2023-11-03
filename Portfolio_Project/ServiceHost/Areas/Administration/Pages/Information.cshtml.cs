5415b86810cff96dd26c650a57b796a4fc65a01a
Maral <mrlmosafercomputer@gmail.com>
2023-11-03 20:37:12 +0330

    Information AdminPart Implemented

---
+++ Portfolio_Project/ServiceHost/Areas/Administration/Pages/Information.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioManagement.Application.Contracts.Information;
+
+namespace ServiceHost.Areas.Administration.Pages
+{
+    public class InformationModel : PageModel
+    {
+        public string Message { get; set; }
+        public InformationViewModel Information { get; set; }
+        public IInformationApplication _informationApplication { get; set; }
+
+        public InformationModel(IInformationApplication informationApplication)
+        {
+            _informationApplication = informationApplication;
+        }
+
+        public void OnGet()
+        {
+            Information = _informationApplication.GetItem();
+        }
+        public RedirectToPageResult OnPost(CreateInformation command)
+        {
+            var result = _informationApplication.Create(command);
+            if (!result.IsSucceeded)
+                Message = result.Message;
+            return RedirectToPage("/Information");
+        }
+    }
+}
