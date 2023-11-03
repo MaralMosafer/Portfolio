ee6910a5afe8681308eec2b8ca8d44b703d29a14
Maral <mrlmosafercomputer@gmail.com>
2023-11-03 20:56:48 +0330

    Add Validation

---
+++ Portfolio_Project/0_Framework/Application/ValidationMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace _0_Framework.Application
+{
+    public class ValidationMessages
+    {
+        public const string IsRequired = "This value cannot be empty";
+        public const string MaxLenght = "The entered value exceeds the allowed length";
+    }
+}
--- Portfolio_Project/PortfolioManagement.Application.Contracts/Information/CreateInformation.cs
+++ Portfolio_Project/PortfolioManagement.Application.Contracts/Information/CreateInformation.cs
@@ -1,14 +1,33 @@
-﻿namespace PortfolioManagement.Application.Contracts.Information
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioManagement.Application.Contracts.Information
 {
     public class CreateInformation
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(80, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(80, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Family { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Biography { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(500, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Address { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(300, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(2000, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Picture { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string PictureAlt { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string PictureTitle { get; set; } = string.Empty;
     }
 }
