c4e52745c3afef220bb2f8bbc45c5803b78d981d
Maral <mrlmosafercomputer@gmail.com>
2023-11-03 17:30:26 +0330

    Add Services

---
+++ Portfolio_Project/ProjectManagement.Configuration/PortfolioManagementBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PortfolioManagement.Application.Contracts.Information;
+using PortfolioManagement.Domain.InformationAgg;
+using ProjectManagement.Infrastructure;
+
+namespace ProjectManagement.Configuration
+{
+    public class PortfolioManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<PortfolioContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddTransient<IInformationApplication, IInformationApplication>();
+            services.AddTransient<IInformationRepository, IInformationRepository>();
+        }
+    }
+}
