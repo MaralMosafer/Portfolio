59ac07d99a81605a4b5a3f75a5d139a5944ac983
Maral <mrlmosafercomputer@gmail.com>
2023-11-04 19:33:19 +0330

    Biography Validation

--- Portfolio_Project/PortfolioManagement.Application.Contracts/Biography/CreateBiography.cs
+++ Portfolio_Project/PortfolioManagement.Application.Contracts/Biography/CreateBiography.cs
@@ -1,15 +1,35 @@
-﻿namespace PortfolioManagement.Application.Contracts.Biography
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioManagement.Application.Contracts.Biography
 {
     public class CreateBiography
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(500, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(160, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Fullname { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(50, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Birthday { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public int Age { get; set; }
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(80, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Languages { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(50, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Nationality { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(80, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Address { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(20, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Mobile { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(50, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Email { get; set; } = string.Empty;
     }
 }
