7e944f497ce62931069468e53171f8f9a232cc05
Maral <mrlmosafercomputer@gmail.com>
2023-11-04 18:26:59 +0330

    BiographyApplication Implemented

---
+++ Portfolio_Project/PortfolioManagement.Application/BiographyApplication.cs
@@ -0,0 +1,50 @@
+﻿using _0_Framework.Application;
+using PortfolioManagement.Application.Contracts.Biography;
+using PortfolioManagement.Domain.BiographyAgg;
+
+namespace PortfolioManagement.Application
+{
+    public class BiographyApplication : IBiographyApplication
+    {
+        private readonly IBiographyRepository _biographyRepository;
+
+        public BiographyApplication(IBiographyRepository biographyRepository)
+        {
+            _biographyRepository = biographyRepository;
+        }
+
+        public OperationResult Create(CreateBiography command)
+        {
+            if (_biographyRepository.CheckDataExists() == false)
+            {
+                var biography = new Biography(command.Description, command.Fullname, command.Birthday, command.Age, command.Languages, command.Nationality, command.Adress, command.Mobile, command.Email);
+                _biographyRepository.CreateAndSave(biography);
+                _biographyRepository.SaveChanges();
+            }
+            var operationResult = new OperationResult();
+            return operationResult.Successful();
+        }
+
+        public OperationResult Edit(EditBiography command)
+        {
+            if (_biographyRepository.CheckDataExists() == true)
+            {
+                var biography = _biographyRepository.GetBy(command.Id);
+                biography.Edit(command.Description, command.Fullname, command.Birthday, command.Age, command.Languages, command.Nationality, command.Adress, command.Mobile, command.Email);
+                _biographyRepository.SaveChanges();
+            }
+            var operationResult = new OperationResult();
+            return operationResult.Successful();
+        }
+
+        public EditBiography GetDetails(long id)
+        {
+            return _biographyRepository.GetDetails(id);
+        }
+
+        public BiographyViewModel GetItem()
+        {
+            return _biographyRepository.GetItem();
+        }
+    }
+}
