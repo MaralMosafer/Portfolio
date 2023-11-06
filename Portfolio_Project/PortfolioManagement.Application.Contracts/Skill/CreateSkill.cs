ca55f49bf95b8c01e5d8b6568e6c76d556affa68
Maral <mrlmosafercomputer@gmail.com>
2023-11-06 13:19:20 +0330

    Skill Validation Added

--- Portfolio_Project/PortfolioManagement.Application.Contracts/Skill/CreateSkill.cs
+++ Portfolio_Project/PortfolioManagement.Application.Contracts/Skill/CreateSkill.cs
@@ -1,8 +1,15 @@
-﻿namespace PortfolioManagement.Application.Contracts.Skill
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioManagement.Application.Contracts.Skill
 {
     public class CreateSkill
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(3, ErrorMessage = ValidationMessages.MaxLenght)]
         public int Value { get; set; }
     }
 }
