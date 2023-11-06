4b13b28259ad16262870563aa49115a6b5438e56
Maral <mrlmosafercomputer@gmail.com>
2023-11-06 14:15:43 +0330

    Service Validation Added

--- Portfolio_Project/PortfolioManagement.Application.Contracts/Service/CreateService.cs
+++ Portfolio_Project/PortfolioManagement.Application.Contracts/Service/CreateService.cs
@@ -1,8 +1,15 @@
-﻿namespace PortfolioManagement.Application.Contracts.Service
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioManagement.Application.Contracts.Service
 {
     public class CreateService
     {
+        [Required(ErrorMessage =ValidationMessages.IsRequired)]
+        [MaxLength(150,ErrorMessage =ValidationMessages.MaxLenght)]
         public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(300, ErrorMessage = ValidationMessages.MaxLenght
         public string Description { get; set; } = string.Empty;
         public float Price { get; set; }
     }
