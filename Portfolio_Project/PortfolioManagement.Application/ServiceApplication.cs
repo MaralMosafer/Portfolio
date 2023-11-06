91d9233411c32a18e1c2cfba2c221e775b5e4823
Maral <mrlmosafercomputer@gmail.com>
2023-11-06 14:09:44 +0330

    ServiceApplication Implemented

---
+++ Portfolio_Project/PortfolioManagement.Application/ServiceApplication.cs
@@ -0,0 +1,56 @@
+﻿using _0_Framework.Application;
+using PortfolioManagement.Application.Contracts.Service;
+using PortfolioManagement.Domain.ServiceAgg;
+using PortfolioManagement.Domain.SkillAgg;
+
+namespace PortfolioManagement.Application
+{
+    public class ServiceApplication : IServiceApplication
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public ServiceApplication(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+
+        public OperationResult Create(CreateService command)
+        {
+            var operationResult = new OperationResult();
+            var service = new Service(command.Title, command.Description, command.Price);
+
+            if (_serviceRepository.Exists(x => x.Title == command.Title))
+                return operationResult.Failed(ApplicationMessages.Duplicated);
+
+            _serviceRepository.CreateAndSave(service);
+            _serviceRepository.SaveChanges();
+            return operationResult.Successful();
+        }
+
+        public OperationResult Edit(EditService command)
+        {
+            var operationResult = new OperationResult();
+            var service = _serviceRepository.GetBy(command.Id);
+
+            if (_serviceRepository.Exists(x => x.Title == command.Title && x.Id != command.Id))
+                return operationResult.Failed(ApplicationMessages.Duplicated);
+
+            if (service == null)
+                return operationResult.Failed(ApplicationMessages.NotFound);
+
+            service.Edit(command.Title, command.Description, command.Price);
+            _serviceRepository.SaveChanges();
+            return operationResult.Successful();
+        }
+
+        public List<ServiceViewModel> GetAll()
+        {
+            return _serviceRepository.GetAll();
+        }
+
+        public EditService GetDetailsBy(long id)
+        {
+            return _serviceRepository.GetDetailsBy(id);
+        }
+    }
+}
