3d4b22dcbca0dbbbaab879130fa96a1b678923e1
Maral <mrlmosafercomputer@gmail.com>
2023-11-06 12:56:30 +0330

    Validation Education&Experience Added

--- Portfolio_Project/PortfolioManagement.Application.Contracts/Education/CreateEducation.cs
+++ Portfolio_Project/PortfolioManagement.Application.Contracts/Education/CreateEducation.cs
@@ -1,11 +1,24 @@
-﻿namespace PortfolioManagement.Application.Contracts.Education
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioManagement.Application.Contracts.Education
 {
     public class CreateEducation
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string University { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(20, ErrorMessage = ValidationMessages.MaxLenght)]
         public string StartDate { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(20, ErrorMessage = ValidationMessages.MaxLenght)]
         public string EndDate { get; set; } = string.Empty;
     }
 }
--- Portfolio_Project/PortfolioManagement.Application.Contracts/Experience/CreateExperience.cs
+++ Portfolio_Project/PortfolioManagement.Application.Contracts/Experience/CreateExperience.cs
@@ -1,11 +1,24 @@
-﻿namespace PortfolioManagement.Application.Contracts.Experience
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioManagement.Application.Contracts.Experience
 {
     public class CreateExperience
     {
-        public string Place { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(100, ErrorMessage = ValidationMessages.MaxLenght)]
+        public string Place { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = ValidationMessages.MaxLenght)]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(20, ErrorMessage = ValidationMessages.MaxLenght)]
         public string StartDate { get; set; } = string.Empty;
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(20, ErrorMessage = ValidationMessages.MaxLenght)]
         public string EndDate { get; set; } = string.Empty;
     }
 }
