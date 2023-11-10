41c6b9870685d14318d542443fb65a0eb9fd251e
Maral <mrlmosafercomputer@gmail.com>
2023-11-11 01:09:38 +0330

    Create LoginForm

---
+++ Portfolio_Project/ServiceHost/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
