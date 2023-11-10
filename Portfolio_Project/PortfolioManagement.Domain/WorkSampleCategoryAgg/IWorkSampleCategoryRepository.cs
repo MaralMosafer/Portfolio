0dca3d4486658ed6c6d1fa338d3f4161e9ca1af9
Maral <mrlmosafercomputer@gmail.com>
2023-11-10 17:12:47 +0330

    WorkSampleCategory Domain implemented

---
+++ Portfolio_Project/PortfolioManagement.Domain/WorkSampleCategoryAgg/IWorkSampleCategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿using _0_Framework.Domain;
+using PortfolioManagement.Domain.WorkSampleCategoryAgg;
+
+namespace PortfolioManagement.Domain.WorkSampleAgg
+{
+    public interface IWorkSampleCategoryRepository:IRepositoryBase<WorkSampleCategory> { }
+}
---
+++ Portfolio_Project/PortfolioManagement.Domain/WorkSampleCategoryAgg/WorkSampleCategory.cs
@@ -0,0 +1,14 @@
+﻿using _0_Framework.Domain;
+
+namespace PortfolioManagement.Domain.WorkSampleCategoryAgg
+{
+    public class WorkSampleCategory : DomainBase
+    {
+        public string Name { get; private set; } = string.Empty;
+
+        public WorkSampleCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
