6981255711834a2c9bf478381c66f2bb15a4c8fc
Maral <mrlmosafercomputer@gmail.com>
2023-11-11 16:59:20 +0330

    ArticleCategory Domain Implemented

---
+++ Portfolio_Project/BlogManagement/BM.Domain/BlogManagement.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,18 @@
+﻿using _0_Framework.Domain;
+
+namespace BlogManagement.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory:DomainBase
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public ArticleCategory(string name)
+        {
+            Name = name;
+        }
+        public void Edit(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Portfolio_Project/BlogManagement/BM.Domain/BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -0,0 +1,6 @@
+﻿using _0_Framework.Domain;
+
+namespace BlogManagement.Domain.ArticleCategoryAgg
+{
+    public interface IArticleCategoryRepository:IRepositoryBase<ArticleCategory> { }
+}
