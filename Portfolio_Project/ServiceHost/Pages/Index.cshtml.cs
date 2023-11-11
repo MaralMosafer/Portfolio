bf8c2380bbfbe719400f39b4f0ea7c37860e953e
Maral <mrlmosafercomputer@gmail.com>
2023-11-12 01:26:12 +0330

    Contact(UI) Implemented

--- Portfolio_Project/ServiceHost/Pages/Index.cshtml.cs
+++ Portfolio_Project/ServiceHost/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactManagement.Application.Contracts.Contact;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IContactApplication _contactApplication;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IContactApplication contactApplication)
         {
-            _logger = logger;
+            _contactApplication = contactApplication;
         }
 
         public void OnGet()
         {
-
+        }
+        public RedirectToPageResult OnPost(AddContact command)
+        {
+            _contactApplication.Add(command);
+            return RedirectToPage("./Index");
         }
     }
 }
